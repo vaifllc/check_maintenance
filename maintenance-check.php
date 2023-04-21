b08fb03840d9c7bbbc719e42543507dc64ea7be0
VAIF R2 <68044749+vaifllc@users.noreply.github.com>
2023-04-20 22:04:29 -0400

    Update maintenance-check.php

--- maintenance-check.php
+++ maintenance-check.php
@@ -1,1 +1,14 @@
-# check_maintenance
+<?php
+
+// Set the header to indicate JSON content
+header('Content-Type: application/json');
+
+// Check if maintenance mode is enabled
+$maintenanceMode = true; // Set to true if maintenance mode is enabled
+
+// Return a JSON response with the maintenance mode status
+echo json_encode(array('maintenance_mode' => $maintenanceMode));
+
+?>
+
+
